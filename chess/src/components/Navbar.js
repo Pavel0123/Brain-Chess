476672316e2fbbbfbb42b389638c0e567d6fe151
Pavel0123 <pavel.suchanek@educanet.cz>
2021-11-18 14:42:12 +0100

    first build

---
+++ chess/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css"
+
+function Navbar() {
+  return(
+    <div className="Navbar__container">
+      <h1>HOME</h1>
+    </div>
+  )
+}
+
+export default Navbar;
